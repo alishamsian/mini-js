32a177b981ad0a686d6bb6f04820a3caa36cff1b
Ali Shamsian <84272102+alishamsian@users.noreply.github.com>
2025-01-03 23:10:16 +0330

    end clock

--- alarm-clock/script.js
+++ alarm-clock/script.js
@@ -1,4 +1,5 @@
 const selectMenu = document.querySelectorAll("select");
+const timeBox = document.querySelector(".time");
 
 for (let i = 23; i >= 0; i--) {
   i = i < 10 ? "0" + i : i;
@@ -17,3 +18,15 @@ for (let i = 59; i >= 0; i--) {
     option.text = i;
     selectMenu[1].appendChild(option);
   }
+
+  setInterval(() => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+  
+    timeBox.textContent = `${hours < 10 ? "0" + hours : hours}:${
+      minutes < 10 ? "0" + minutes : minutes
+    }:${seconds < 10 ? "0" + seconds : seconds}`;
+
+  } , 1000);
