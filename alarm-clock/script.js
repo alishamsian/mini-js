2e52fd6472a058a26a7696dd1ea94a42ae3bc546
Ali Shamsian <84272102+alishamsian@users.noreply.github.com>
2025-01-03 23:22:28 +0330

    end

--- alarm-clock/script.js
+++ alarm-clock/script.js
@@ -1,5 +1,6 @@
 const selectMenu = document.querySelectorAll("select");
 const timeBox = document.querySelector(".time");
+const setAlarm = document.querySelector(".set-alarm");
 
 for (let i = 23; i >= 0; i--) {
   i = i < 10 ? "0" + i : i;
@@ -30,3 +31,24 @@ for (let i = 59; i >= 0; i--) {
     }:${seconds < 10 ? "0" + seconds : seconds}`;
 
   } , 1000);
+
+  setAlarm.addEventListener("click", () => {
+    const selectedHours = selectMenu[0].value;
+    const selectedMinutes = selectMenu[1].value;
+  
+
+    console.log(selectedHours, selectedMinutes);
+
+    if (selectedHours === "Hour" || selectedMinutes === "Minute") {
+      alert("Please select the time");
+      return;
+    }
+  
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+  
+    if (selectedHours == hours && selectedMinutes == minutes) {
+      alert("Time to wake up!");
+    }
+  } );
